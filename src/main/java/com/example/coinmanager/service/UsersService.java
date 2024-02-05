011d765bb6210225af25bd2ce7c217041f37b2cc
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-02-05 19:19:06 -0300

    feat/ UserService created

--- src/main/java/com/example/coinmanager/service/UsersService.java
+++ src/main/java/com/example/coinmanager/service/UsersService.java
@@ -1,5 +1,19 @@
 package com.example.coinmanager.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.coinmanager.repository.UsersRepository;
+import com.example.coinmanager.model.Users;
+import com.example.coinmanager.dto.UsersDto;
+
+@Service
 public class UsersService {
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public Users createUser(UsersDto data) {
+        Users user = new Users(data);
+        return usersRepository.save(user);
+    }
 
 }
