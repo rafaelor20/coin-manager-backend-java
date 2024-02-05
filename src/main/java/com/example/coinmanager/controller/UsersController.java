c2fb0560270722c5442a210245654f5e5a703df6
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-02-05 19:27:16 -0300

    feat/ create User Controller

--- src/main/java/com/example/coinmanager/controller/UsersController.java
+++ src/main/java/com/example/coinmanager/controller/UsersController.java
@@ -1,5 +1,23 @@
 package com.example.coinmanager.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.coinmanager.service.UsersService;
+import com.example.coinmanager.dto.UsersDto;
+
+@RestController
+@RequestMapping()
 public class UsersController {
+    @Autowired
+    private UsersService usersService;
+
+    @PostMapping("/signup")
+    public void createUser(@Valid @RequestBody UsersDto req) {
+        usersService.createUser(req);
+    }
 
 }
